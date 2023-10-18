2935c04797bc37a1dac5d9322f2cb353ac22f762
AASHISH PATHAK <116142970+AashishPathak1@users.noreply.github.com>
2023-10-18 10:19:29 +0530

    Update RightAngleTriangle.java

--- RightAngleTriangle.java
+++ RightAngleTriangle.java
@@ -4,7 +4,7 @@ public class RightAngleTriangle {
     public static void main(String[] args) {
         System.out.println("Enter any number :");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt()
+        int n = sc.nextInt();
         for (int index = 1; index <=n; index++) {
             for (int j = 1; j <=index; j++) {
                 System.out.print("*");
